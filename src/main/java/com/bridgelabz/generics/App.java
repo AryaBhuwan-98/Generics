41685b5d49f852637c6d8d2c8ad5446839f92b0b
AryaBhuwan-98 <aryabhuwan873@gmail.com>
2022-11-26 19:52:06 -0800

    adding program to find max of integers,float and String for more than three parameters

--- src/main/java/com/bridgelabz/generics/App.java
+++ src/main/java/com/bridgelabz/generics/App.java
@@ -1,13 +1,7 @@
 package com.bridgelabz.generics;
-
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
+public class App {
+	    public static void main( String[] args )
+	    {
+	        System.out.println( "Welcome To Java Generics" );
+	    }
+	}
